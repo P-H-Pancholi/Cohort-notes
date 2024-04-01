424dba8c8dd0654155ef3dec041f4e14147784e3
P-H-Pancholi <pancholiphalin@outlook.com>
2024-04-01 23:29:44 +0530

    Added notes for express & body-parser

--- week-2/HTTP-Server-1/index.js
+++ week-2/HTTP-Server-1/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 // npm install express
 
+const bodyParser = require("body-parser")
+// npm install body-parser
+
 const app = express()
 const port = 3000
+
+app.use(bodyParser.json())
+
+app.post('/sample', (req,  res) => {
+  console.log(req.body)
+  res.send('Hello World!')
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
